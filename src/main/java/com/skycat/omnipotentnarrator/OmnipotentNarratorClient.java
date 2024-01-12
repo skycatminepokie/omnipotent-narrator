40d547c542d45086db088ccedb8a3f01f77ffa51
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-01-11 19:49:51 -0800

    Project creation

---
+++ src/main/java/com/skycat/omnipotentnarrator/OmnipotentNarratorClient.java
@@ -0,0 +1,16 @@
+package com.skycat.omnipotentnarrator;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OmnipotentNarratorClient implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("omnipotent-narrator");
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
